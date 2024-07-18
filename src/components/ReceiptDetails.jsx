52d8b6cd46816a5af39cc386153f201cbf8df980
deepakdhakad07 <dhakad.deepak@webseeder.in>
2024-07-18 23:01:36 +0530

    space2

--- src/components/ReceiptDetails.jsx
+++ src/components/ReceiptDetails.jsx
@@ -87,7 +87,7 @@ const ReceiptDetails = ({ token }) => {
             className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             onClick={printPDF}
           >
-            Download PDF
+             Download PDF
           </button>
         </div>
       </div>
